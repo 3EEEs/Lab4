252d26f2bded4e70931276355e0c20bfb20097b8
Ethan <ethan8short@gmail.com>
2023-10-19 11:16:25 -0700

    Update

--- StringHash.cpp
+++ StringHash.cpp
@@ -2,3 +2,28 @@
 // Created by Student on 10/19/2023.
 //
 
+#include "StringHash.hpp"
+
+StringHash::StringHash(int size = defaultSize) {
+
+}
+
+StringHash::~StringHash() {
+
+}
+
+void StringHash::addItem(int value) {
+
+}
+
+bool StringHash::findItem(int value) {
+
+}
+
+void removeItem(int value) {
+
+}
+
+string displayTable() {
+
+}
--- StringHash.hpp
+++ StringHash.hpp
@@ -5,19 +5,38 @@
 #ifndef LAB4_STRINGHASH_HPP
 #define LAB4_STRINGHASH_HPP
 
+#include <iostream>
+
+using namespace std;
+
+//Regular text
+#define BLK "\e[0;30m"
+#define RED "\e[0;31m"
+#define GRN "\e[0;32m"
+#define YEL "\e[0;33m"
+#define BLU "\e[0;34m"
+#define MAG "\e[0;35m"
+#define CYN "\e[0;36m"
+#define WHT "\e[0;37m"
+
+//Reset
+#define reset "\e[0m"
+
 class StringHash {
 private:
     int defaultSize = 11;
 
+    int *theArray;
+
 public:
 
-    StringHash()::StringHash();
+    StringHash(int size = defaultSize);
 
-    StringHash::~StringHash();
+    ~StringHash();
 
     void addItem(value);
 
-    void findItem(value);
+    bool findItem(value);
 
     void removeItem(value);
 
